320f5757c9d271c5998cf7c16a95de59821be2b0
AKHIL DHINGRA <dhingra93akhil@gmail.com>
2021-04-20 02:39:36 +0530

    first commit

---
+++ ShopBridge/Controllers/CategoryController.cs
@@ -0,0 +1,110 @@
+﻿using ShopBridgeDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShopBridge.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Category> LoadAllCategories()
+        {
+            using (InventoryDBEntities inventoryDBEntities = new InventoryDBEntities())
+            {
+                return inventoryDBEntities.Categories.ToList();
+            }
+        }
+        [HttpGet]
+        public HttpResponseMessage LoadAllCategoriesByID(int ID)
+        {
+            using (InventoryDBEntities inventoryDBEntities = new InventoryDBEntities())
+            {
+                var entity = inventoryDBEntities.Categories.FirstOrDefault(Cat => Cat.Id == ID);
+                if (entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Category with ID =" + ID.ToString() + " not exists!");
+                }
+            }
+        }
+        [HttpPost]
+        public HttpResponseMessage CreateCategory([FromBody] Category category)
+        {
+            try
+            {
+                using (InventoryDBEntities inventoryEntity = new InventoryDBEntities())
+                {
+                    inventoryEntity.Categories.Add(category);
+                    inventoryEntity.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, category);
+                    message.Headers.Location = new Uri(Request.RequestUri + category.Id.ToString());
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+        [HttpDelete]
+        public HttpResponseMessage DeleteCategoryByID(int ID)
+        {
+            try
+            {
+                using (InventoryDBEntities inventoryEntity = new InventoryDBEntities())
+                {
+                    var entity = inventoryEntity.Categories.FirstOrDefault(Prod => Prod.Id == ID);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Category with ID " + ID.ToString() + " not found to be deleted!");
+                    }
+                    else
+                    {
+                        inventoryEntity.Categories.Remove(entity);
+                        inventoryEntity.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+        [HttpPut]
+        public HttpResponseMessage ModifyCategories(int ID, [FromBody] Category category)
+        {
+            try
+            {
+                using (InventoryDBEntities inventoryEntity = new InventoryDBEntities())
+                {
+                    var entity = inventoryEntity.Categories.FirstOrDefault(cat => cat.Id == ID);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Category with ID = " + ID.ToString() + " not found!");
+                    }
+                    else
+                    {
+                        entity.Category_Name = category.Category_Name;
+                        inventoryEntity.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+
+}
---
+++ ShopBridge/Controllers/ProductsController.cs
@@ -0,0 +1,114 @@
+﻿using ShopBridgeDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShopBridge.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Product> LoadAllProducts()
+        {
+            using (InventoryDBEntities inventoryDBEntities = new InventoryDBEntities())
+            {
+                return inventoryDBEntities.Products.ToList();
+            }
+        }
+        [HttpGet]
+        public HttpResponseMessage LoadAllProductsByID(int ID)
+        {
+            using (InventoryDBEntities inventoryDBEntities = new InventoryDBEntities())
+            {
+                var entity = inventoryDBEntities.Products.FirstOrDefault(Prod => Prod.Id == ID);
+                if (entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Product with ID =" + ID.ToString() + " not exists!");
+                }
+            }
+        }
+        [HttpPost]
+        public HttpResponseMessage CreateProducts([FromBody] Product product)
+        {
+            try
+            {
+                using (InventoryDBEntities inventoryEntity = new InventoryDBEntities())
+                {
+                    inventoryEntity.Products.Add(product);
+                    inventoryEntity.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, product);
+                    message.Headers.Location = new Uri(Request.RequestUri + product.Id.ToString());
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+        [HttpDelete]
+        public HttpResponseMessage DeleteProductsByID(int ID)
+        {
+            try
+            {
+                using (InventoryDBEntities inventoryEntity = new InventoryDBEntities())
+                {
+                    var entity = inventoryEntity.Products.FirstOrDefault(Prod => Prod.Id == ID);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Product with ID " + ID.ToString() + " not found to be deleted!");
+                    }
+                    else
+                    {
+                        inventoryEntity.Products.Remove(entity);
+                        inventoryEntity.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+        [HttpPut]
+        public HttpResponseMessage ModifyProducts(int ID, [FromBody] Product product)
+        {
+            try
+            {
+                using (InventoryDBEntities inventoryEntity = new InventoryDBEntities())
+                {
+                    var entity = inventoryEntity.Products.FirstOrDefault(prod => prod.Id == ID);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Product with ID = " + ID.ToString() + " not found!");
+                    }
+                    else
+                    {
+                        entity.ProductName = product.ProductName;
+                        entity.ProductDescription = product.ProductDescription;
+                        entity.Quantity_Available = product.Quantity_Available;
+                        entity.Price = product.Price;
+                        entity.Category_Id = product.Category_Id;
+                        inventoryEntity.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+
+}
