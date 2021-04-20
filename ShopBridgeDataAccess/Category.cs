137b49eca202cdee0772eb8a31d5529d6e69d46e
AKHIL DHINGRA <dhingra93akhil@gmail.com>
2021-04-20 19:44:29 +0530

    Removed extra namespaces

--- ShopBridgeDataAccess/Category.cs
+++ ShopBridgeDataAccess/Category.cs
@@ -9,9 +9,8 @@
 
 namespace ShopBridgeDataAccess
 {
-    using System;
     using System.Collections.Generic;
-    
+
     public partial class Category
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
